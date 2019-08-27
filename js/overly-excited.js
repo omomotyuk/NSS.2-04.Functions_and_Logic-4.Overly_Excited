e89c757e9f4ec63a8e519746874a918fba254b4c
Alex <o.a.momotyuk@gmail.com>
2019-08-27 09:30:36 -0500

    Function Arguments practice - done

--- js/overly-excited.js
+++ js/overly-excited.js
@@ -1,16 +1,16 @@
 /*
-The learning objects of this exercise is to use iteration and string concatenation
+// The learning objects of this exercise is to use iteration and string concatenation
 
-Create an addExcitement function that should console.log() rows of words. It should take an array containing the words of a sentence and output them in the developer console.
+// Create an addExcitement function that should console.log() rows of words. It should take an array containing the words of a sentence and output them in the developer console.
 */
 // Create an array that contains the words in the sentence
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
 /*
-    The addExcitement function should be an impure function, and accept the array as the sole argument. It should iterate over the array and output the words to the browser console.
+// The addExcitement function should be an impure function, and accept the array as the sole argument. It should iterate over the array and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
 
+function addExcitement (theWordArray, characterArg ) {
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
 
@@ -18,10 +18,14 @@ function addExcitement (theWordArray) {
         // Concatenate the new word onto buildMeUp
         buildMeUp += " " + theWordArray[i];
 
-        /* Add logic to addExcitement that places an exclamation point (!) after every third word. Read the following English statement and write the equivalent in JavaSript code to make it work.
+        /*
+        // Add logic to addExcitement that places an exclamation point (!) after every third word. Read the following English statement and write the equivalent in JavaSript code to make it work.
+        */
+        /*
+        // Add a new argument to the function so that a developer can specify which character should be displayed instead of it always being an exclamation point.
         */
         if ( (i + 1) % 3 === 0 ) {
-            buildMeUp += "!";
+            buildMeUp += characterArg ;
         }
 
         // Print buildMeUp to the console
@@ -30,4 +34,5 @@ function addExcitement (theWordArray) {
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence)
+// I want to use a question mark
+addExcitement(sentence, "?");
